308ce72cc9ab505f655e83e1bcf41aea14c483bd
snehapal2012 <snehapal2012@gmail.com>
2022-07-19 22:17:06 +0530

    Added: Welcome to Employee Payroll Service

---
+++ src/main/java/com/employeepayrollservice/EmployeePayrollServiceMain.java
@@ -0,0 +1,7 @@
+package com.employeepayrollservice;
+
+public class EmployeePayrollServiceMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Service");
+    }
+}
